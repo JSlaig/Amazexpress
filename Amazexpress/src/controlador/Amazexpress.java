cf065fd64df9d1f41880cfe8a051ccbcdd986659
jsalig00 <jsalig00@estudiantes.unileon.es>
2021-01-05 18:36:45 +0100

    0.12 Arreglados problemas con main por el uso de una libreria deprecada

--- Amazexpress/src/controlador/Amazexpress.java
+++ Amazexpress/src/controlador/Amazexpress.java
@@ -5,31 +5,32 @@ import java.io.IOException;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
 import javafx.fxml.FXMLLoader;
-import javafx.stage.Stage;
-import modelo.AmazexpressApp;
-import sun.applet.Main;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import modelo.AmazexpressApp;
 
 
 public class Amazexpress extends Application {
 	
 	@Override
 	public void start(Stage primaryStage) {	
-		
 		try {
+		
 			FXMLLoader loader = new FXMLLoader();
-			loader.setLocation(Main.class.getResource("/vista/MainUI.fxml"));
+			loader.setLocation(getClass().getResource("/vista/MainUI.fxml"));
 			Pane ventana = (Pane) loader.load();
 			
-			//Show the scene containing the root layout
 			Scene scene = new Scene(ventana);
 			primaryStage.setScene(scene);
 			primaryStage.show();
 			
 		}catch(IOException e) {
-			System.out.println(e.getMessage());
-		}		
+			e.printStackTrace();
+		}
+
+		
 	}
 	
 	public static void main(String[] args) {	
