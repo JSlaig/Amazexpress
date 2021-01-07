0c13a8b5a932f2167c90af9700ba1ef685b51859
bvalem00 <36730111+bvalem00@users.noreply.github.com>
2021-01-07 18:57:24 +0100

    Update Producto.java

--- Amazexpress/src/modelo/Producto.java
+++ Amazexpress/src/modelo/Producto.java
@@ -2,4 +2,47 @@ package modelo;
 
 public class Producto {
 
+//Atributos de producto
+	public int idProducto;
+	public boolean vendido;
+	
+//Constructor
+	public Producto(String nombre, String estado, String descripcion, String vendedor, String comprador, boolean vendido) {
+		this.nombre = nombre;
+		this.estado = estado;
+		this.descripcion = descrip;
+		this.vendedor = vendedor;
+		this.comprador = comprador;
+		this.vendido = vendido;
+	}
+
+//Getters
+	public int getIdProducto() {
+		return this.idProducto;
+	}
+	
+	public boolean getVendido() {
+		return this.vendido;
+	}
+	
+    public String getNombre() {
+
+        return this.nombre;
+    }
+
+    public String getEstado(){
+        return this.estado;
+    }
+    
+    public String getDescripcion(){
+        return this.descripcion;
+    }
+
+    public String getVendedor(){
+        return this.vendedor;
+    }
+
+    public String getComprador(){
+        return this.comprador;
+    }
 }
